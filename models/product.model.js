1c3f2acbaeb4860b8d54eb8d10014f0c0cf0c718
subash ebanezer <subashebanezer@gmail.com>
2024-10-21 16:01:39 +0530

    first commit

---
+++ models/product.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter product name"],
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
